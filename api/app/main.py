9296b38f82aac95bb8ac5bed2c79c20b79a95806
elkumah <e.fkumah@gmail.com>
2026-06-25 00:44:25 +0100

    feat(schema, main.py): Add URL request schema and /shorten endpoint for request body

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+from .schemas import URLRequest
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World. Testing Fast API endpoint."}
+
+@app.post("/shorten/")
+async def shorten_url(url_request: URLRequest):
+    url = url_request.url
+    return {"short_code": "abc123",  "shortened_url": f"http://short.ly/abc123"}
