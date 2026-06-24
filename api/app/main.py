91aec3b6ea2361c9928be01d659da06b10c7124d
elkumah <e.fkumah@gmail.com>
2026-06-25 00:48:17 +0100

    Modified(schema): Add URL reponse model

--- api/app/main.py
+++ api/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from .schemas import URLRequest
+from .schemas import URLResponse
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World. Testing Fast API endpoint."}
 
-@app.post("/shorten/")
+@app.post("/shorten/",response_model=URLResponse)
 async def shorten_url(url_request: URLRequest):
     url = url_request.url
-    return {"short_code": "abc123",  "shortened_url": f"http://short.ly/abc123"}
+    return URLResponse(short_code="abc123", shortened_url=f"http://short.ly/abc123")
--- api/app/schemas.py
+++ api/app/schemas.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel
 
 class URLRequest(BaseModel):
     url: str
+
+class URLResponse(BaseModel):
+    short_code: str
+    shortened_url: str
