5cc3018811addfa723d5b62b8c5c44ffe3a473c8
elkumah <e.fkumah@gmail.com>
2026-07-02 22:34:48 +0000

    Feat(secure short code): Generate a cryptographically secure URL-safe short code

--- api/app/services/url_service.py
+++ api/app/services/url_service.py
@@ -1,5 +1,8 @@
+import secrets
 class URLService:
     def generate_short_code(self, url: str) -> str:
-       
-        short_code = "xyz1236"  # Replace this with your actual logic
+# The URL will be used later when checking for duplicate URLs and storing mappings in PostgreSQL.
+        # Generate a cryptographically secure URL-safe short code.
+        SHORT_CODE_BYTE = 6  # Length of the short code
+        short_code = secrets.token_urlsafe(SHORT_CODE_BYTE) 
         return short_code
