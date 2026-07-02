8bacfa1c1d2bec95be622ad94001027c6936337e
elkumah <e.fkumah@gmail.com>
2026-07-02 13:14:10 +0000

    Fix(validation): Import  Http validation rule to validate the url

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
+from pydantic import HttpUrl
 
+# Define a Pydantic model for the request body
 class URLRequest(BaseModel):
     url: HttpUrl
 
 class URLResponse(BaseModel):
     short_code: str
     shortened_url: str
+
