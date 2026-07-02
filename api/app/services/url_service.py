cfa2367c23a48bbea2f4aca4c2c24eefe0cf94e3
elkumah <e.fkumah@gmail.com>
2026-07-02 21:19:24 +0000

    refactor(short code): Improve the urlservice to only generate the short code

--- api/app/services/url_service.py
+++ api/app/services/url_service.py
@@ -2,5 +2,4 @@ class URLService:
     def generate_short_code(self, url: str) -> str:
        
         short_code = "xyz123"  # Replace this with your actual logic
-        shortened_url = f"http://localhost:8000/{short_code}"
-        return shortened_url
+        return short_code
