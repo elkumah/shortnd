d1a47d5947897135c696c40fd44f8a3799969629
elkumah <e.fkumah@gmail.com>
2026-07-02 13:12:29 +0000

    Improvement(removed business logic): Removed logic to generate short code to the service layer

--- api/app/main.py
+++ api/app/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
-from .schemas import URLRequest
-from .schemas import URLResponse
+from .schemas import URLRequest, URLResponse
+from .services.url_service import URLService
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World. Testing Fast API endpoint."}
 
-@app.post("/shorten/",response_model=URLResponse)
+
+@app.post("/shorten/", response_model=URLResponse)
 async def shorten_url(url_request: URLRequest):
     url = url_request.url
-    return URLResponse(short_code="abc123", shortened_url=f"http://short.ly/abc123")
+    url_service = URLService()
+    shortened_url = url_service.generate_short_code(url)
+    # Extract the short code from the shortened URL
+    short_code = shortened_url.split("/")[-1]
+    return URLResponse(short_code=short_code, shortened_url=shortened_url)
