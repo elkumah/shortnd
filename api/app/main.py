0777e74d91b49747515dbfcd06a08f8bea3c66d0
elkumah <e.fkumah@gmail.com>
2026-07-02 21:37:12 +0000

    Refactor(logic): remove business logic from the main.py

--- api/app/main.py
+++ api/app/main.py
@@ -12,7 +12,5 @@ def root():
 async def shorten_url(url_request: URLRequest):
     url = url_request.url
     url_service = URLService()
-    shortened_url = url_service.generate_short_code(url)
-    # Extract the short code from the shortened URL
-    short_code = shortened_url.split("/")[-1]
-    return URLResponse(short_code=short_code, shortened_url=shortened_url)
+    short_code = url_service.generate_short_code(url)
+    return URLResponse(short_code=short_code, shortened_url=f"http://localhost:8000/{short_code}")
