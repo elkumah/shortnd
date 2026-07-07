31afc2967c19d1b2a680daf45978aa9992d146e7
elkumah <e.fkumah@gmail.com>
2026-07-07 22:54:24 +0000

    Feat(repository): Implement URLRepository for managing URL mappings in the database

---
+++ api/app/repositories/url_repository.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+from sqlachemy import select
+from ..models import URL
+
+class URLRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    def create(self,url: URL) -> URL:
+        self.db.add(url)
+        self.db.commit()
+        self.db.refresh(url)
+        return new_url
+
+    def get_by_short_code(self, short_code: str) -> Optional[URL]:
+        # Retrieve a URL mapping from the database based on the provided short code.
+        statement = select(URL).where(URL.short_code == short_code)
+        result = self.db.execute(statement)
+        return result.scalar_one_or_none()
+
+    def exists_by_short_code(short_code: str) -> bool:
+        # Check if a URL mapping with the given short code exists in the database.
+        statement = select(URL).where(URL.short_code == short_code)
+        result = self.db.execute(statement)
+        return result.scalar_one_or_none() is not None
