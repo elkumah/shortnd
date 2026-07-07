8a41f351879d40d7e44388e06d65f34beb2f51cb
elkumah <e.fkumah@gmail.com>
2026-07-07 23:23:57 +0000

    Feat(service): Refactor URLService to include repository initialization and streamline short URL creation

--- api/app/services/url_service.py
+++ api/app/services/url_service.py
@@ -1,8 +1,20 @@
 import secrets
+from app.models import URL
+from app.repositories.url_repository import URLRepository
+
 class URLService:
-    def generate_short_code(self, url: str) -> str:
-# The URL will be used later when checking for duplicate URLs and storing mappings in PostgreSQL.
-        # Generate a cryptographically secure URL-safe short code.
+    def __init__(self, repository: URLRepository):
+
+        self.repository = repository
+    def generate_short_code(self) -> str:
+        # Generate a random URL-safe short code
         SHORT_CODE_BYTE = 6  # Length of the short code
         short_code = secrets.token_urlsafe(SHORT_CODE_BYTE) 
         return short_code
+
+    def create_short_url(self, original_url: str) -> URL:
+        # Create a new URL mapping in the database using the URLRepository.
+        short_code = self.generate_short_code()
+        new_url = URL(original_url=original_url, short_code=short_code)
+        saved_url = self.repository.create(new_url)
+        return saved_url
