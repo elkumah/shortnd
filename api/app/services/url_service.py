95bbdfd17b58a85d08889bdfab07d9e68d2a80ed
elkumah <e.fkumah@gmail.com>
2026-07-09 11:12:41 +0000

    Refactor: Consolidate short code length definition and streamline URL creation in URLService

--- api/app/services/url_service.py
+++ api/app/services/url_service.py
@@ -2,19 +2,21 @@ import secrets
 from app.models import URL
 from app.repositories.url_repository import URLRepository
 
+SHORT_CODE_BYTES = 6  # Length of the short code
+
 class URLService:
     def __init__(self, repository: URLRepository):
 
         self.repository = repository
     def generate_short_code(self) -> str:
         # Generate a random URL-safe short code
-        SHORT_CODE_BYTE = 6  # Length of the short code
-        short_code = secrets.token_urlsafe(SHORT_CODE_BYTE) 
+        
+        short_code = secrets.token_urlsafe(SHORT_CODE_BYTES) 
         return short_code
 
     def create_short_url(self, original_url: str) -> URL:
         # Create a new URL mapping in the database using the URLRepository.
         short_code = self.generate_short_code()
-        new_url = URL(original_url=original_url, short_code=short_code)
-        saved_url = self.repository.create(new_url)
-        return saved_url
+        url= URL(original_url=original_url, short_code=short_code)
+        return self.repository.create(url)
+      
