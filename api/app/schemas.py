b0d5e741f468afc74aab0ce288f1fa839f54127e
elkumah <e.fkumah@gmail.com>
2026-07-16 12:23:30 +0000

    Add Pydantic model for original URL response retrieval

--- api/app/schemas.py
+++ api/app/schemas.py
@@ -9,3 +9,7 @@ class URLResponse(BaseModel):
     short_code: str
     shortened_url: str
 
+# Define a Pydantic model for the request response when retrieving the original URL
+class OriginalURLResponse(BaseModel):
+    short_code: str
+    original_url: HttpUrl
