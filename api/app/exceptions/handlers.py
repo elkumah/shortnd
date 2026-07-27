34f6bec135625fac98d27e1766d3600ec9abc980
elkumah <e.fkumah@gmail.com>
2026-07-27 19:39:37 +0000

    Add logging for URLNotFoundException handling in exception handler

--- api/app/exceptions/handlers.py
+++ api/app/exceptions/handlers.py
@@ -1,11 +1,18 @@
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from .custom_exceptions import URLNotFoundException
+from ..logging.logger import get_logger
+
+
+
+logger = get_logger(__name__)
 
 async def url_not_found_exception_handler(request: Request, exc: URLNotFoundException):
     """
     Handle URLNotFoundException and return a JSON response with error details.
+    Logs the error and returns a JSON response with a 404 status code.
     """
+    logger.error(f"URL not found: {exc.short_code}")
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND,
         content={
