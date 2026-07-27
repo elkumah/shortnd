bd504fbdf37ff17a9890376a60b6fd6998df4a38
elkumah <e.fkumah@gmail.com>
2026-07-27 20:19:04 +0000

    Fix import formatting in handlers.py by removing unnecessary blank line

--- api/app/exceptions/handlers.py
+++ api/app/exceptions/handlers.py
@@ -2,7 +2,7 @@ from fastapi import Request, status
 from fastapi.responses import JSONResponse
 from .custom_exceptions import URLNotFoundException
 from ..logging.logger import get_logger
-
+import logging
 
 
 logger = get_logger(__name__)
