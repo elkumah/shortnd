6660e92824df7ddb3cb20f5050c82b75ab18bc55
elkumah <e.fkumah@gmail.com>
2026-08-06 22:19:48 +0000

    Add tests for URL shortening functionality and validation errors

--- api/tests/api/test_url_endpoints.py
+++ api/tests/api/test_url_endpoints.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
-
+from fastapi import status
+from app.config import settings
 from app.main import app
 
 client = TestClient(app)
@@ -24,3 +25,50 @@ def test_health_check_endpoint():
     data = response.json()
 
     assert data["status"] == "healthy"
+
+def test_shorten_url_successfully_creates_short_url():
+    payload = {
+        "url": "https://example.com"
+    }
+    response = client.post("/shorten/", json=payload)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    data = response.json()
+
+    assert "short_code" in data
+    assert "shortened_url" in data
+
+    assert data["short_code"] != ""
+    assert data["shortened_url"].startswith(settings.BASE_URL)
+
+def test_shorten_url_returns_422_for_invalid_url():
+    payload = {
+        "url": "invalid-url"
+    }
+    response = client.post("/shorten/", json=payload)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    data = response.json()
+    assert "detail" in data
+
+    assert data["detail"][0]["loc"] == ["body", "url"]
+
+def test_shorten_url_returns_422_when_url_field_is_missing():
+    payload = {}
+
+    response = client.post(
+        "/shorten/",
+        json=payload
+    )
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    data = response.json()
+
+    assert "detail" in data
+
+    assert data["detail"][0]["loc"] == ["body", "url"]
+
+    assert data["detail"][0]["type"] == "missing"
